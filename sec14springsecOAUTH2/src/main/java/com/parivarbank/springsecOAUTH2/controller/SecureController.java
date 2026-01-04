147e72d9018a8c5ac729702b2c6d21a11bb24e63
Ramya <ramyapentela0620@gmail.com>
2026-01-03 19:42:48 -0500

    Convert sec14springsecOAUTH2 from submodule to normal folder

---
+++ sec14springsecOAUTH2/src/main/java/com/parivarbank/springsecOAUTH2/controller/SecureController.java
@@ -0,0 +1,20 @@
+package com.parivarbank.springsecOAUTH2.controller;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecureController {
+    @GetMapping("/secure")
+    public String secure(Authentication authentication){
+        if(authentication instanceof UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken){
+            System.out.println(usernamePasswordAuthenticationToken);
+        } else if (authentication instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+            System.out.println(oAuth2AuthenticationToken);
+        }
+        return "secure.html";
+    }
+}
